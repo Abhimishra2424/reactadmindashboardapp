c1bd7361aaeffcecdf7e6fb1531f71121fe124a8
Abhishek Mishra <abmishra700@gmail.com>
2023-01-08 20:16:44 +0530

    Sidebar added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import { ColorModeContext, useMode } from "./theme";
+import { Route, Routes } from "react-router-dom";
 import { CssBaseline, ThemeProvider } from "@mui/material";
 import Tobbar from "./screens/global/Tobbar";
+import Sidebar from './screens/global/Sidebar'
+import Dashboard from "./screens/dashboard";
+// import Team from "./screens/team";
+// import Invoices from "./screens/invoices";
+// import Contacts from "./screens/contacts";
+
 
 function App() {
   const [theme, colorMode] = useMode();
@@ -9,8 +16,12 @@ function App() {
       <ThemeProvider theme={theme}>
         <CssBaseline />
         <div className="app">
+          <Sidebar />
           <main className="content">
-             <Tobbar />
+            <Tobbar />
+            <Routes>
+              <Route path="/" element={<Dashboard />} />
+            </Routes>
           </main>
         </div>
       </ThemeProvider>
