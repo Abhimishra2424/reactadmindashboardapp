12fdcdd364ec799be54e343f2e58d1ae50ae542a
Abhishek Mishra <abmishra700@gmail.com>
2023-01-08 18:30:20 +0530

    file structure

---
+++ src/screens/global/Tobbar.jsx
@@ -0,0 +1,24 @@
+import { Box, IconButton, useTheme } from "@mui/material";
+import { useContext } from "react";
+import { ColorModeContext, tokens } from "../../theme";
+import InputBase from "@mui/material/InputBase";
+import LightModeOutlinedIcon from "@mui/icons-material/LightModeOutlined";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import NotificationsOutlinedIcon from "@mui/icons-material/NotificationsOutlined";
+import SettingsOutlinedIcon from "@mui/icons-material/SettingsOutlined";
+import PersonOutlinedIcon from "@mui/icons-material/PersonOutlined";
+import SearchIcon from "@mui/icons-material/Search";
+
+const Tobbar = () => {
+  const theme = useTheme();
+  const colors = tokens(theme.palette.mode);
+  const colorMode = useContext(ColorModeContext);
+
+  return (
+    <Box display={"flex"} justifyContent="space-between" p={2}>
+      Tobbar
+    </Box>
+  );
+};
+
+export default Tobbar;
