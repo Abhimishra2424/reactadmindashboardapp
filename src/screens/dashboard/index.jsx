090850acaee44b62143b894eb04955e3f58afc9a
Abhishek Mishra <abmishra700@gmail.com>
2023-01-28 09:53:27 +0530

    Added Header comp

--- src/screens/dashboard/index.jsx
+++ src/screens/dashboard/index.jsx
@@ -1,9 +1,18 @@
-
+import { Box } from "@mui/material";
+import Header from "../../components/Header";
 
 const DashBoard = () => {
   return (
-    <div>index</div>
-  )
-}
+    <Box m="20px">
+      <Box
+        display={"flex"}
+        justifyContent="space-between"
+        alignItems={"center"}
+      >
+        <Header title={"DASHBOARD"} subtitle={"welcome to your dashboard"} />
+      </Box>
+    </Box>
+  );
+};
 
-export default DashBoard
+export default DashBoard;
